bb048390d0aafd9b3e23244ceaedaee045bab17a
zbcjaso <zbcjaso@ZBC-S-6BN0W>
2021-09-02 09:38:37 +0200

    Add project files.

---
+++ Library/Book.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        public Book(string title, int bookNumber, bool available, string author)
+        {
+            this.title = title;
+            this.bookNumber = bookNumber;
+            this.available = available;
+            this.author = author;
+        }
+
+        private string title;
+        private int bookNumber;
+        private bool available;
+        private string author;
+
+        #region getAndSet
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+        public int BookNumber
+        {
+            get
+            {
+                return bookNumber;
+            }
+            set
+            {
+                bookNumber = value;
+            }
+        }
+        public bool Available
+        {
+            get
+            {
+                return available;
+            }
+            set
+            {
+                available = value;
+            }
+        }
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Library/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book hp = new Book("Harry Potter", 1, true, "J.K. Rowling");
+            Book er = new Book("Eragon", 2, true, "Christopher Paolini");
+            Book lr = new Book("Lord of the Rings", 3, true, "J.R.R Tolkien");
+            Book gt = new Book("Game of Thrones", 4, true, "George R.R. Martin");
+
+            List<Book> books = new List<Book>() { hp, er, lr, gt };
+            Stack<Book> userLoan = new Stack<Book>() { };
+            string remove = "y";
+            while (books.Count > 0 && remove.Contains("y"))
+            {
+                Console.WriteLine("Available books:");
+                for (int i = 0; i < books.Count; i++)
+                {
+                    if (books.ElementAt(i).Available == true)
+                    {
+                        Console.WriteLine(books.ElementAt(i).Title);
+                    }
+                }
+                Console.WriteLine("Write the title on the book you want to borrow");
+                string toStack = Console.ReadLine().ToLower();
+                if (toStack == "harry potter")
+                {
+                    books.Remove(hp);
+                    userLoan.Push(hp);
+                }
+                else if (toStack == "eragon")
+                {
+                    books.Remove(er);
+                    userLoan.Push(er);
+                }
+                else if (toStack == "lord of the rings")
+                {
+                    books.Remove(lr);
+                    userLoan.Push(lr);
+                }
+                else if (toStack == "game of thrones")
+                {
+                    books.Remove(gt);
+                    userLoan.Push(gt);
+                }
+                else
+                {
+                    Console.WriteLine("This book doesn't exist in our library");
+                }
+                Console.Clear();
+                Console.WriteLine("Your stack:");
+                for (int i = 0; i < userLoan.Count; i++)
+                {
+                    Console.WriteLine(userLoan.ElementAt(i).Title);
+                }
+                if (books.Count > 0)
+                {
+                    Console.WriteLine("Do you want to borrow another book?");
+                    remove = Console.ReadLine().ToLower();
+                }
+                Console.Clear();
+            }
+            for (int i = 0; i < userLoan.Count; i++)
+            {
+                Console.WriteLine($"You have borrowed {userLoan.Peek().Title} from the library");
+                userLoan.Pop();
+                i = -1;
+            }
+        }
+    }
+}
